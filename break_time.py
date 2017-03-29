ed8b249173f242de24cb9c3c1cf649d50ff06bdb
clucier <onedudedesign@gmail.com>
2017-03-29 09:31:41 -0700

    Fixed the comment wierdness, need a space after the # otherwise get a warning in Pycharm

--- break_time.py
+++ break_time.py
@@ -4,15 +4,15 @@ import time
 break_num = 0
 num_breaks = 3
 
-#indicate when the program started
+# indicate when the program started
 print('Program Started at :' + time.ctime())
 
-#loop for three breaks
+# loop for three breaks
 while break_num < num_breaks:
-    #sleep at 10 seconds for testing
+    # sleep at 10 seconds for testing
     #TODO change to one hour for production
     time.sleep(10)
-    #indicate the breaktime
+    # indicate the breaktime
     print ('Take a Break At :' + time.ctime())
     webbrowser.open("https://www.youtube.com/watch?v=m9AT7H4GGrA")
     break_num = break_num + 1
