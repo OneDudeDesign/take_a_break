d96f83c83f606f6a4987aa659edaf572be04d155
clucier <onedudedesign@gmail.com>
2017-03-29 09:29:25 -0700

    Fix the time prints and add comments and todos

--- break_time.py
+++ break_time.py
@@ -4,13 +4,16 @@ import time
 break_num = 0
 num_breaks = 3
 
-print('Program Started at :')
-print time.ctime()
+#indicate when the program started
+print('Program Started at :' + time.ctime())
 
+#loop for three breaks
 while break_num < num_breaks:
+    #sleep at 10 seconds for testing
+    #TODO change to one hour for production
     time.sleep(10)
-    print ('Take a Break At')
-    print time.ctime()
+    #indicate the breaktime
+    print ('Take a Break At' + time.ctime())
     webbrowser.open("https://www.youtube.com/watch?v=m9AT7H4GGrA")
     break_num = break_num + 1
 
